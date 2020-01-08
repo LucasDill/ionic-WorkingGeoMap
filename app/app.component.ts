001afe8fad009ff6e8f7c34bd720926dd1ca58c1
LucasDill <ldillist@lakeheadu.ca>
2020-01-08 12:50:34 -0500

    1 file modified Changed Back

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,13 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import {AfterViewInit} from '@angular/core';
-import { TabsPage } from '../pages/tabs/tabs';
-import { AboutPage } from '../pages/about/about';
 
+import { TabsPage } from '../pages/tabs/tabs';
+//import { AboutPage } from '../pages/about/about';// delete to go back to tabs 
 @Component({
   templateUrl: 'app.html'
 })
-
-
-export class MyApp implements AfterViewInit {
-
-  ngAfterViewInit(): void {
-    // Load google maps script after view init
-    const DSLScript = document.createElement('script');
-    //DSLScript.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC2GRIwOatzPmiamkpv3znVK8hi9g4lGoU'; // replace by your API key
-    DSLScript.type = 'text/javascript';
-    document.body.appendChild(DSLScript);
-    document.body.removeChild(DSLScript);
-  }
-
-  rootPage:any = AboutPage;
+export class MyApp {
+  rootPage:any = TabsPage;//change to TabsPage to go back to tabs
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
